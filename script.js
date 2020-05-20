b191e4f066491af14c242849e545ac0bdd58315d
unknown <you@example.com>
2020-05-20 20:24:09 +0100

    include localStorage

--- script.js
+++ script.js
@@ -46,9 +46,24 @@ function handleCHange(event) {
   const value = event.target.value;
 
   handleStyle[name](value);
+  saveValues(name, value);
   showCss();
 
 }
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach(propertie => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+    showCss()
+  })
+}
+setValues();
 
 function showCss() {
   cssText.innerHTML = "<span>" + btn.style.cssText.split("; ").join(";</pan><span>")
